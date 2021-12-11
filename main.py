ce929d338534d6e74fa2ebe3328a80cfefa8ded1
dsanabri <dsanabri@umich.edu>
2021-12-10 21:12:01 -0500

    removed unneccessary import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """Main file to change and set lights."""
 import time
 import os
-import requests
 from dotenv import load_dotenv  # pylint: disable=import-error
 from classes.wled import Wled
 from classes.weather import Weather
